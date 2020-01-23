9c28a4a226b7268262960ae08b6bb47d75f73554
Rajan <rajansaini691@gmail.com>
2020-01-22 17:15:33 -0800

    Get system of equations

--- hw2.py
+++ hw2.py
@@ -7,7 +7,7 @@ Assignment description:
     - [done] Manually identify a set of corresponding points
       - [done] Color selected dots
     - Solve the homography problem (compute the H matrix)
-      - Create a system of equations
+      - [done] Create a system of equations
       - Solve using solution of minimal norm
     - Apply the transformation on one of the images in the pair to synthesize
       the other image, and vice versa
@@ -22,7 +22,49 @@ Report should contain:
 """
 import sys
 import getopt
+import numpy as np
 from helpers import RawImage, ginput
+
+
+def get_H(original, transformed):
+    """
+    Calculates the H matrix using the given coordinate pairs
+
+    Parameters:
+        original            Coordinates from the original image, that we are
+                            trying to distort
+        transformed         Coordinates from the transformed image, functioning
+                            as our target
+    """
+    # The A matrix
+    A = []
+
+    # The b vector
+    b = []
+
+    # p and p_prime are individual coordinates
+    for p, p_prime in zip(original, transformed):
+        # Points from the original image
+        x = p[0]
+        y = p[1]
+
+        # Points from the transformed image
+        x_prime = p_prime[0]
+        y_prime = p_prime[1]
+
+        # Populate A
+        A_i = [[x, y, 1, 0, 0, 0, -x*x_prime, -y*y_prime],
+               [0, 0, 0, x, y, 1, -x*y_prime, -y*y]]
+        A += A_i
+
+        # Populate b
+        b += [x_prime, y_prime]
+
+    # Convert to np array
+    A = np.array(A)
+    b = np.array(b)
+
+    # Solve for H
 
 
 if __name__ == "__main__":
@@ -51,9 +93,15 @@ if __name__ == "__main__":
 
     """
     Get coordinates
+    (note to grader: RawImage and ginput are my own functions, implemented in
+    helper.py)
     """
     left = RawImage(leftimage, num_points, "left")
     right = RawImage(rightimage, num_points, "right")
 
     coords = ginput(left, right)
-    print(coords)
+
+    """
+    Calculate H Matrix
+    """
+    get_H(coords[0], coords[1])
